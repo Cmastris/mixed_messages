86ff5aa567de9922cedfb3373d1a99ec62ac3c8c
Chris Mastris <cmastris@hotmail.com>
2023-05-28 19:19:38 +0100

    Add function to generate statement objects

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 Every time a user runs a program, they should get a new, randomized output.
 The message that it outputs should be made up of at least three different pieces of data.
 */
+
+
+function statementFactory(statement, sentiment) {
+    /* Generate statement objects.
+    Params:
+        statement (str): the statement text.
+        sentiment (int): a representation of how positive/optimistic the statement is.
+        -2 is very bad; -1 is bad; 0 is neutral; 1 is positive; 2 is very positive.
+    */
+   return {
+    statement,
+    sentiment
+   };
+}
+
